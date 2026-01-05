478cc129eab910854fcfcb5f139cc57ef7c75382
DrakonDev <nicollaskaiubi416@gmail.com>
2026-01-05 16:33:06 -0300

    Add whitelist and blacklist configuration

---
+++ resources/whitelist.lua
@@ -0,0 +1,18 @@
+return {
+    whitelistOwner = {
+        [10067326164] = true,
+    },
+
+    whitelistAdmin = {
+        [2222222] = true,
+    },
+
+    whitelist = {
+        [33333333] = true,
+        [44444444] = true,
+    },
+
+    blacklist = {
+        [55555555] = true,
+    }   
+}
